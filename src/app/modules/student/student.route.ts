65824b1b810e584fdf62acc10b7e21f98cb36e00
Aminul667 <rahathssn@gmail.com>
2024-05-21 16:37:14 +0600

    added for single student

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -5,3 +5,9 @@ const router = express.Router();
 
 // it calls controller
 router.post('/create-student', StudentControllers.createStudent);
+
+router.get('/', StudentControllers.getAllStudents);
+
+router.get('/:studentId', StudentControllers.getSingleStudent);
+
+export const StudentRoutes = router; // as router is an object itself
