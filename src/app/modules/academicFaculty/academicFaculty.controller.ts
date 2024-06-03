7caf42f0bce5497f929bd43b6200b11f5042d771
Aminul667 <rahathssn@gmail.com>
2024-06-03 22:42:09 +0600

    checked all the routes

--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -42,7 +42,7 @@ const updateAcademicFaculty = catchAsync(async (req, res) => {
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Academic Faculty is retrieved successfully',
+    message: 'Academic Faculty is updated successfully',
     data: result,
   });
 });
