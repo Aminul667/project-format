069e367c7ee8257be4aac5a31b5f781b18807d13
Aminul667 <rahathssn@gmail.com>
2024-06-03 21:11:12 +0600

    added accademic semester routes, controller and services

---
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,66 @@
+import httpStatus from 'http-status';
+
+import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
+import { AcademicFacultyServices } from './academicFaculty.service';
+
+const createAcademicFaculty = catchAsync(async (req, res) => {
+  //   const { password, student: studentData } = req.body;
+
+  const result = await AcademicFacultyServices.createAcademicFacultyIntoDb(
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty is created successfully',
+    data: result,
+  });
+});
+
+const getSingleAcademicFaculty = catchAsync(async (req, res) => {
+  const { facultyId } = req.params;
+  const result =
+    await AcademicFacultyServices.getSingleAcademicFacultyFromDB(facultyId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty is retrieved successfully',
+    data: result,
+  });
+});
+
+const updateAcademicFaculty = catchAsync(async (req, res) => {
+  const { facultyId } = req.params;
+  const result = await AcademicFacultyServices.updateAcademicFacultyIntoDB(
+    facultyId,
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty is retrieved successfully',
+    data: result,
+  });
+});
+
+const getAllAcademicFaculties = catchAsync(async (req, res) => {
+  const result = await AcademicFacultyServices.getAllAcademicFacultiesFromDB();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty are retrieved successfully',
+    data: result,
+  });
+});
+
+export const AcademicFacultyControllers = {
+  createAcademicFaculty,
+  getSingleAcademicFaculty,
+  updateAcademicFaculty,
+  getAllAcademicFaculties,
+};
