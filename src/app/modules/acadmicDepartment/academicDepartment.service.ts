8b58a36c445368914544c660730c9fe237a150ff
Aminul667 <rahathssn@gmail.com>
2024-06-04 23:29:12 +0600

    added populate and extended error

--- src/app/modules/acadmicDepartment/academicDepartment.service.ts
+++ src/app/modules/acadmicDepartment/academicDepartment.service.ts
@@ -2,20 +2,13 @@ import { TAcademicDepartment } from './academicDepartment.interface';
 import { AcademicDepartment } from './academicDepartment.model';
 
 const createAcademicDepartmentIntoDb = async (payload: TAcademicDepartment) => {
-  // const isDepartmentExist = await AcademicDepartment.findOne({
-  //   name: payload.name,
-  // });
-
-  // if (isDepartmentExist) {
-  //   throw new Error('This department is already exist');
-  // }
-
   const result = await AcademicDepartment.create(payload);
   return result;
 };
 
 const getSingleAcademicDepartmentFromDB = async (id: string) => {
-  const result = await AcademicDepartment.findById(id);
+  const result =
+    await AcademicDepartment.findById(id).populate('academicFaculty');
   return result;
 };
 
@@ -34,7 +27,7 @@ const updateAcademicDepartmentIntoDB = async (
 };
 
 const getAllAcademicDepartmentFromDB = async () => {
-  const result = await AcademicDepartment.find();
+  const result = await AcademicDepartment.find().populate('academicFaculty');
   return result;
 };
 
