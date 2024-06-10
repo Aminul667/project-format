fbcfd314be92d590ccd87b78da9936025ba2e2b6
Aminul667 <rahathssn@gmail.com>
2024-06-10 17:16:52 +0600

    added refactoring to students

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -5,8 +5,8 @@ import { StudentServices } from './student.service';
 import catchAsync from '../../utils/catchAsync';
 
 const getSingleStudent: RequestHandler = catchAsync(async (req, res) => {
-  const { studentId } = req.params;
-  const result = await StudentServices.getSingleStudentFromDB(studentId);
+  const { id } = req.params;
+  const result = await StudentServices.getSingleStudentFromDB(id);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -28,10 +28,10 @@ const getAllStudents: RequestHandler = catchAsync(async (req, res) => {
 });
 
 const updateStudent: RequestHandler = catchAsync(async (req, res) => {
-  const { studentId } = req.params;
+  const { id } = req.params;
   const { student } = req.body;
 
-  const result = await StudentServices.updateStudentIntoDB(studentId, student);
+  const result = await StudentServices.updateStudentIntoDB(id, student);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -42,8 +42,8 @@ const updateStudent: RequestHandler = catchAsync(async (req, res) => {
 });
 
 const deleteStudent: RequestHandler = catchAsync(async (req, res) => {
-  const { studentId } = req.params;
-  const result = await StudentServices.deleteStudentFromDB(studentId);
+  const { id } = req.params;
+  const result = await StudentServices.deleteStudentFromDB(id);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
