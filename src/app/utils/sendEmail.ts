44781f7b254dc127a5225bc639c1ebb8d478ddc3
aminul <rahathssn@gmail.com>
2024-10-07 22:26:48 +0600

    19-3 Send email using nodemailer when hitting forget pasword route

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -1,21 +1,22 @@
 import nodemailer from 'nodemailer';
+import config from '../config';
 
-export const sendEmail = async () => {
+export const sendEmail = async (to: string, html: string) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
+    host: config.smtp_host,
     port: 587,
-    secure: false, // true for port 465, false for other ports
+    secure: config.NODE_ENV === 'production', // true for port 465, false for other ports
     auth: {
-      user: 'maddison53@ethereal.email',
-      pass: 'jn7jnAPss4f63QBp6D',
+      user: config.smtp_user,
+      pass: config.smtp_pass,
     },
   });
 
   await transporter.sendMail({
-    from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
-    to: 'bar@example.com, baz@example.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Hello world?</b>', // html body
+    from: config.smtp_user, // sender address
+    to,
+    subject: 'Reset your password within ten min', // Subject line
+    text: '', // plain text body
+    html,
   });
 };
