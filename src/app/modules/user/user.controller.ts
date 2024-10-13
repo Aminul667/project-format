1131fe6caa3df36c2e7a1519588ea018065638d0
aminul <rahathssn@gmail.com>
2024-10-13 15:50:08 +0600

    change status route

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -46,13 +46,15 @@ const createAdmin = catchAsync(async (req, res) => {
 });
 
 const getMe = catchAsync(async (req, res) => {
-  const token = req.headers.authorization;
+  // const token = req.headers.authorization;
 
-  if (!token) {
-    throw new AppError(httpStatus.NOT_FOUND, 'Token not found!');
-  }
+  // if (!token) {
+  //   throw new AppError(httpStatus.NOT_FOUND, 'Token not found!');
+  // }
 
-  const result = await UserServices.getMe(token);
+  const { userId, role } = req.user;
+
+  const result = await UserServices.getMe(userId, role);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -62,9 +64,23 @@ const getMe = catchAsync(async (req, res) => {
   });
 });
 
+const changeStatus = catchAsync(async (req, res) => {
+  const id = req.params.id;
+
+  const result = await UserServices.changeStatus(id, req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Status is updated successfully',
+    data: result,
+  });
+});
+
 export const UserControllers = {
   createStudent,
   createFaculty,
   createAdmin,
   getMe,
+  changeStatus,
 };
